c43125a78edf6fbb09f64e4e4f9deaf0bc2f146d
SsrCoder <SsrCoder@gmail.com>
2024-01-13 17:21:58 +0800

    add cmp, copilot, flash

--- init.lua
+++ init.lua
@@ -9,6 +9,9 @@ plugin "plugins.mason"
 plugin "plugins.mason-lspconfig"
 plugin "plugins.schemastore"
 plugin "plugins.lspconfig"
+plugin "plugins.cmp"
+plugin "plugins.flash"
+plugin "plugins.copilot"
 
 require "basic.lazy"
 
---
+++ lua/plugins/flash.lua
@@ -0,0 +1,12 @@
+local M = {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	keys = {
+	},
+}
+
+function M.config()
+	require("flash").setup{}
+end
+
+return M
