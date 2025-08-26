bec5b282026b72b85831b708da04409ef5978077
SsrCoder <SsrCoder@gmail.com>
2025-08-26 19:53:03 +0800

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,15 +28,19 @@ vim.opt.sidescrolloff = 10
 -- vim.opt.list = true
 
 vim.wo.cursorline = true
+vim.opt.colorcolumn = { 100 }
 
-vim.g.clipboard = {
-	name = 'OSC 52',
-	copy = {
-		['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-		['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-	},
-	paste = {
-		['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-		['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-	},
-}
+local hasOSC52, _ = pcall(require, "vim.ui.clipboard.osc52")
+if hasOSC52 then
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+			['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+		},
+	}
+end
