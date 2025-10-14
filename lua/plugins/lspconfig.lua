cb0b3b6a21cc2d4962a1a19740664c11c52c2bb4
SsrCoder <SsrCoder@gmail.com>
2025-10-14 20:48:57 +0800

    Update lspconfig.lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,6 @@
 local M = {
 	"neovim/nvim-lspconfig",
 	dependencies = { "saghen/blink.cmp", "lukas-reineke/lsp-format.nvim" },
-	-- dependencies = { "saghen/blink.cmp" },
 	event = { "BufReadPre", "BufNewFile" },
 }
 
@@ -23,36 +22,33 @@ function M.config()
 	})
 
 	local capabilities = require("blink.cmp").get_lsp_capabilities()
-	local lspconfig = require("lspconfig")
 
-	local on_attach = require("lsp-format").on_attach
-
+	vim.lsp.config('*', {
+		capabilities = capabilities,
+	})
 
 	for _, value in ipairs(support_lsp) do
-		lspconfig[value].setup({ capabilities = capabilities, on_attach = on_attach })
+		vim.lsp.enable(value, true)
 	end
-
-
-	-- lspconfig['lua_ls'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['gopls'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['clangd'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['jsonls'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['astro'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['cssls'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['ts_ls'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['tailwindcss'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['rust_analyzer'].setup({ capabilities = capabilities, on_attach = on_attach })
-	-- lspconfig['lua_ls'].setup({ capabilities = capabilities })
 
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 		callback = function(ev)
+			local bufnr = ev.buf
+			local client = vim.lsp.get_client_by_id(ev.data.client_id)
+			if not client then
+				return
+			end
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover)
 			vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float,
 				{ buffer = ev.buf, desc = "[LSP] Show diagnostic" })
 			vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
 			vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action,
 				{ desc = "[LSP] Show Code Actions", buffer = ev.buf, noremap = true, silent = true })
+
+			local on_attach = require("lsp-format").on_attach
+			on_attach(client, bufnr)
 		end,
 	})
 end
