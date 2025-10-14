7a2d26ce3721a5f813cf1b2e003c5f2215c03ca8
SsrCoder <SsrCoder@gmail.com>
2025-10-14 20:50:12 +0800

    Create nvim-various-textobjs.lua

---
+++ lua/plugins/nvim-various-textobjs.lua
@@ -0,0 +1,12 @@
+---@See: https://github.com/chrisgrieser/nvim-various-textobjs
+local M = {
+	"chrisgrieser/nvim-various-textobjs",
+	event = "VeryLazy",
+	opts = {
+		keymaps = {
+			useDefaults = true,
+		},
+	},
+}
+
+return M
